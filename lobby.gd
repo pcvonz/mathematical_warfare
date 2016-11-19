85f5c5f1737d73848a390e5bc7db85b687b807ac
Paul Von Zimmerman <pcvonz@gmail.com>
2016-11-19 14:33:16 -0800

    Adding gamestate stuff

--- lobby.gd
+++ lobby.gd
@@ -1,15 +1,15 @@
-extends Node2D
+extends Control
 
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
 
 func _ready():
-	get_tree().connect("network_peer_connected", self, "player_connected")
-	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
-	get_tree().connect("connected_to_server", self, "_connected_ok")
-	get_tree().connect("connection_failed", self, "_connected_failed")
-	get_tree().connect("server_disconnected", self, "_server_disconnected")
+	gamestate.connect("connection_failed", self, "_on_connection_failed")
+	gamestate.connect("connection_succeeded", self, "_on_connection_success")
+	gamestate.connect("player_list_changed", self, "refresh_lobby")
+	gamestate.connect("game_ended", self, "_on_game_ended")
+	gamestate.connect("game_error", self, "_on_game_error")
 
 # Player info, associate ID to data
 var player_info = {}
@@ -31,7 +31,26 @@ func _server_disconnected():
 
 func _connected_failed():
 	pass
+
+func _on_join_pressed():
+	if (get_node("ip").get_text() == ""):
+		return
 	
+	var ip = get_node("ip").get_text()
+	
+	get_node("host").set_disabled(true)
+	get_node("join").set_disabled(true)
+	
+	var name = get_node("name").get_text()
+	gamestate.join_game(ip, name)
+		
+func _on_host_pressed():
+	get_node("host").set_disabled(true)
+	get_node("join").set_disabled(true)
+	
+	var name = get_node("name").get_text()
+	gamestate.host_game(name)
+
 remote func register_player(id, info):
 	player_info[id] = info
 	if(get_tree().is_network_server()):
