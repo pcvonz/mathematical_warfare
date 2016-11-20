272f6df05f53abb45b50b8b25da3960d8a89377d
Paul Von Zimmerman <pcvonz@gmail.com>
2016-11-20 14:35:03 -0800

    Adding bases

---
+++ base.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var enemy_team
+var health = 20
+
+func setup(enemy_team):
+	self.enemy_team = enemy_team
+
+func _ready():
+	get_node("Area2D").connect("body_enter", self, "damage_base")
+	get_node("Label").set_text(str(health))
+	
+func damage_base(body):
+	if(body.is_in_group(enemy_team)):
+		if health > 0:
+			body.rpc("kill_self")
+			rpc("do_damage")
+		else:
+			print("game_over")
+	
+#Right now this does damage for each client connected (two clients = 1 damage)
+sync func do_damage():
+	health -= .5
+	get_node("Label").set_text(str(health))
+	
+	
+	
---
+++ base_1.gd
@@ -0,0 +1,8 @@
+extends "res://base.gd"
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	setup("team_2")
