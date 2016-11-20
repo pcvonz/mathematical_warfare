41b9462b04d843e6f7557e5f49ed41d107ecd00c
Paul Von Zimmerman <pcvonz@gmail.com>
2016-11-19 19:08:05 -0800

    Merging and add network_stuff

---
+++ player_1.gd
@@ -0,0 +1,8 @@
+extends "res://player.gd"
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	set_variables("team_1", "team_2", "../../wp_1")
---
+++ player_2.gd
@@ -0,0 +1,8 @@
+extends "res://player.gd"
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	set_variables("team_2", "team_1", "../../wp_2")
